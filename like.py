588d4b0a7703836bb6e6844f626321925c325d72
Furkan <afy@mail.com.tr>
2023-09-30 16:26:22 +0300

    Update like.py

--- like.py
+++ like.py
@@ -1,19 +1,35 @@
-from atproto import Client
+from atproto import Client, models
+import time
 
 def main(client: Client, handle: str):
     print(f"\n {handle}'s posts")
+    
     count = 0
 
-    get_profile = client.bsky.feed.get_author_feed({'actor': handle, 'limit': 100})
-    for profile_view in get_profile.feed:
+    profile_feed = client.app.bsky.feed.get_author_feed({'actor': handle})
+    for feed_view in profile_feed.feed:
         count = count + 1
-        print(count, '-', profile_view.post.record.text)
-        agent.like(profile_view.post)
+        print(count, '-', feed_view.post.record.text)
+        try:
+            client.like(models.create_strong_ref(feed_view.post))
+        except Exception as e:
+            print(f"Hata: {e}")
+            time.sleep(60)
+            client = recreate_session()
+
+def create_session():
+    at_client = Client()
+    at_client.login('handle', 'password')
+    return at_client
+
+def recreate_session():
+    print("Yeniden oturum oluşturuluyor...")
+    time.sleep(60)
+    return create_session()
 
 if __name__ == '__main__':
-    agent = Client()
-    agent.login('bluesky-handle', 'bluesky-password')
-  
+    at_client = create_session()
+
     while True:
         input_handle = input('\nEnter the username of the profile whose posts you want to like: ')
-        main(agent, input_handle)
+        main(at_client, input_handle)
